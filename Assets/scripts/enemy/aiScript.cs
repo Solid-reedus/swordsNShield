f5e6d0d6191bc04e9ef61d0c670beb5dbb1c08d7
Solid-reedus <75674268+Solid-reedus@users.noreply.github.com>
2023-01-12 19:04:35 +0100

    added enemy ai and colorcoating

---
+++ Assets/scripts/enemy/aiScript.cs
@@ -0,0 +1,132 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aiScript : MonoBehaviour, IdamageAble
+{
+    [SerializeField] private GameObject armour;
+    [SerializeField] private GameObject shield;
+    [SerializeField] private Material enemyMaterial;
+
+
+    private int enemyStateVal = 1;
+    //private int enemyWeaponEquip = 1;
+    //string tag = "";
+    [SerializeField] private float health = 100;
+    SpawnManager SpawnManager;
+
+    bool canWalk = true;
+    bool canSwing = true;
+    bool canBlock = true;
+
+    string enemyTag;
+
+
+    private void Awake()
+    {
+        SpawnManager = FindObjectOfType<SpawnManager>();
+
+        if (tag == "ally")
+        {
+            enemyTag = "enemy";
+        }
+        else if (tag == "enemy")
+        {
+            enemyTag = "ally";
+        }
+        else
+        {
+            Debug.LogError($"{this.name} isnt asignt a side");
+        }
+        Colorcoat();
+    }
+
+    void Colorcoat()
+    {
+        if (this.tag == "ally")
+        {
+            shield.transform.GetChild(0).gameObject.SetActive(true);
+            shield.transform.GetChild(1).gameObject.SetActive(false);
+            return;
+        }
+
+        armour.GetComponent<SkinnedMeshRenderer>().material = enemyMaterial;
+        shield.transform.GetChild(0).gameObject.SetActive(false);
+        shield.transform.GetChild(1).gameObject.SetActive(true);
+
+
+        //shield.transform.GetChild(1);
+        //shield.GetComponent<MeshRenderer>().material = enemyMaterial;
+    }
+
+    
+    IEnumerator GoToEnemy(GameObject target)
+    {
+
+
+
+        yield break;
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        KnightState();
+    }
+
+    public void damage(float dmg)
+    {
+        health -= dmg;
+    }
+
+    private void KnightState()
+    {
+        switch (enemyStateVal)
+        {
+            // idle
+            case 1:
+            {
+                if (SpawnManager.BattleIsOver(enemyTag))
+                {
+                    break;
+                }
+                enemyStateVal = 2;
+                break;
+            }
+            // chase
+            case 2:
+            {
+                StartCoroutine(GoToEnemy(
+                    SpawnManager.GetClosestEnemy(this.transform, enemyTag)));
+
+                // go position of the player unit it hits a trigger
+                break;
+            }
+            // swing sword
+            case 3:
+            {
+                // after a random amount of seconds start a couroutine to swing a sword 
+                break;
+            }
+            // block attack
+            case 4:
+            {
+                // start a couroutine when a sword is swung after a random amount of seconds to block  
+                break;
+            }
+            // die
+            case 5:
+            {
+                // when the enemy health is 0 or less the enemy dies
+                break;
+            }
+
+
+        }
+
+
+    }
+
+    
+}
---
+++ Assets/scripts/manager/SpawnManager.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    int enemyCount = 10;
+    int allyCount  = 3;
+
+    [SerializeField] private Transform enemySpawnA;
+    [SerializeField] private Transform enemySpawnB;
+
+    [SerializeField] private Transform AllySpawnA;
+    [SerializeField] private Transform AllySpawnB;
+
+    [SerializeField] private GameObject Knight;
+
+
+    public List<GameObject> enemies = new List<GameObject>();
+    public List<GameObject> allies = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        Vector3 lenghtA = new Vector3(spawnA.position.x , 0 , spawnA.position.z);
+        Vector3 lenghtB = new Vector3(spawnB.position.x , 0 , spawnA.position.z);
+
+        Vector3 widthA = new Vector3(spawnA.position.x , 0 , spawnA.position.z);
+        Vector3 widthB = new Vector3(spawnA.position.x , 0 , spawnB.position.z);
+
+        Debug.Log($"height of spawn {Vector3.Distance(lenghtA, lenghtB)} and with is {Vector3.Distance(widthA, widthB)}");
+        Debug.Log("the surface of the spawn is" + Vector3.Distance(lenghtA, lenghtB) * Vector3.Distance(widthA, widthB));
+        */
+
+        SpawnKnight("enemy", enemyCount, enemySpawnA.position, enemySpawnB.position);
+        SpawnKnight("ally", allyCount, AllySpawnA.position, AllySpawnB.position);
+
+
+        GameObject[] allEnemies = GameObject.FindGameObjectsWithTag("enemy");
+        GameObject[] allAllies = GameObject.FindGameObjectsWithTag("ally");
+
+        foreach (var item in allEnemies)
+        {
+            enemies.Add(item);
+        }
+        foreach (var item in allAllies)
+        {
+            allies.Add(item);
+        }
+
+        int count = 0;
+        foreach (var item in enemies)
+        {
+            count++;
+            //Debug.Log($"num {count} enemy {enemies} pos {item.transform.position}");
+        }
+    }
+
+    public bool BattleIsOver(string enemyTag)
+    {
+        bool battleIsOver = true;
+        foreach (var item in enemies)
+        {
+            if (item != null && item.tag == enemyTag)
+            {
+                battleIsOver = false;
+                return battleIsOver;
+            }
+        }
+        return battleIsOver;
+    }
+
+    private void SpawnKnight(string tagName, int amount, Vector3 spawnB1, Vector3 spawnB2)
+    {
+        List<Vector3> spawnList = new List<Vector3>();
+
+        Knight.tag = tagName;
+        for (int i = 0; i < amount; i++)
+        {
+            Instantiate(Knight, makeNewSpawn(), Quaternion.Euler(0,0,0));
+        }
+
+        Vector3 makeNewSpawn()
+        {
+            float dis;
+            bool isOccupied = false;
+            float posZ = Random.Range(spawnB1.z, spawnB2.z);
+            float posX = Random.Range(spawnB1.x, spawnB2.x);
+            Vector3 spawn = new Vector3(posX, 0, posZ);
+
+            foreach (var item in spawnList)
+            {
+                dis = Vector3.Distance(spawn, item);
+                if (dis > 1)
+                {
+                    isOccupied = true;
+                    break;
+                }
+            }
+            if (isOccupied == true)
+            {
+                makeNewSpawn();
+            }
+            return spawn;
+        }
+
+    }
+
+    public GameObject GetClosestEnemy(Transform instigator, string tagName)
+    {
+        float closest = 100;
+        float tempVal;
+        GameObject target = null;
+
+        foreach (var item in enemies)
+        {
+            tempVal = Vector3.Distance(instigator.position, item.transform.position);
+            if (closest < tempVal && item.tag == tagName)
+            {
+                closest = tempVal;
+                target = item;
+            }
+        }
+        return target;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
